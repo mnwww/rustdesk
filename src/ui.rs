f4c7c274e3c3db8302ab146067eb6e793aba6772
mnwww <yyhome@gmail.com>
2023-11-17 23:59:52 +0800

    Update ui.rs

--- src/ui.rs
+++ src/ui.rs
@@ -105,6 +105,11 @@ pub fn start(args: &mut [String]) {
         frame.event_handler(UI {});
         frame.sciter_handler(UIHostHandler {});
         page = "install.html";
+    } else if args[0] == "--cm" {
+        frame.register_behavior("connection-manager", move || {
+            Box::new(cm::SciterConnectionManager::new())
+        });
+        page = "cm.html";
     } else if (args[0] == "--connect"
         || args[0] == "--file-transfer"
         || args[0] == "--port-forward"
