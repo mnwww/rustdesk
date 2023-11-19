a003c0e6b037b1783d62fcba3c2ed1f42806cb86
mnwww <149638651+mnwww@users.noreply.github.com>
2023-11-19 08:20:43 +0800

    更新 core_main.rs

--- src/core_main.rs
+++ src/core_main.rs
@@ -1,4 +1,5 @@
-*#[cfg(not(any(target_os = "android", target_os = "ios")))]
+use std::process::Command;
+cfg(not(any(target_os = "android", target_os = "ios")))]
 use crate::client::translate;
 #[cfg(not(debug_assertions))]
 #[cfg(not(any(target_os = "android", target_os = "ios")))]
